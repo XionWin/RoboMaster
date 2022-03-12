8fd931a41199b5dcc670d853ee2102c9cced09b3
XionWin <xionwin@hotmail.com>
2022-03-12 04:18:49 +0000

    Update

--- Core/App/Inc/global.h
+++ Core/App/Inc/global.h
@@ -10,10 +10,12 @@ extern "C"
 
 #include "aRGB_led.h"
 #include "console.h"
-
+    // Resources
     extern aRGB_led_t ARGB_LED;
     extern console_t CONSOLE;
 
+    // Stm32f4 functions
+    /* Register in USART1_IRQHandler */
     void USART1_Rx_Idle_Callback(UART_HandleTypeDef *huart);
 
 #ifdef __cplusplus
